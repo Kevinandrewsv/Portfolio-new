8aee1077778c78abdf07d0a92df273ea8e889a41
kevin andrews <kevinandrews001@gmail.com>
2025-08-04 14:44:58 +0530

    new change

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import glsl from 'vite-plugin-glsl'
+
 
 export default defineConfig({
   plugins: [
     react(),
-    glsl(), // enables importing/inlining GLSL shader code
   ],
 })
